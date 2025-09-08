6243669c2467051ed42679b9a153fcf037fcada9
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2025-09-08 10:17:43 +0200

    fix(print): 🐛 enforce light mode for printing to ensure readability
    Updated print styles to guarantee a light theme for better visibility, improving selector specificity and reducing the need for !important declarations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
+import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, envField } from "astro/config";
 import icon from "astro-icon";
 import { BASE_URL } from "./src/configs/site.consts.ts";
 import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from "./src/i18n/locales";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -90,7 +90,7 @@ export default defineConfig({
 			},
 		}),
 		(await import("astro-compress")).default({
-			CCSS: true,
+			CSS: true,
 			HTML: {
 				"html-minifier-terser": {
 					removeAttributeQuotes: false,
